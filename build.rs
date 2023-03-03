65f5d1b95f0939421ba2e0e0f74a2eb6e8270eac
Lily Mara <lilymara@fastmail.com>
2023-03-03 08:45:44 -0800

    Support paging through directory of images

--- build.rs
+++ build.rs
@@ -1,6 +1,28 @@
+use std::{fs::File, io::Write, path::PathBuf};
+
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
     embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
+
+    println!("cargo:rerun-if-changed=images");
+
+    let out_path = PathBuf::from(std::env::var("OUT_DIR")?).join("images.rs");
+
+    let mut outfile = File::create(out_path)?;
+
+    outfile.write_all(b"pub(crate) const IMAGE_DATA: &[&[u8]] = &[\n")?;
+
+    for result in glob::glob("./images/*.bmp")? {
+        let path = result?.canonicalize()?;
+
+        outfile.write_fmt(format_args!(
+            "    include_bytes!(\"{}\"),\n",
+            path.display()
+        ))?;
+    }
+
+    outfile.write_all(b"];\n")?;
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 use display_interface::WriteOnlyDataCommand;
 use display_interface_spi::SPIInterfaceNoCS;
-use embedded_graphics::{
-    draw_target::DrawTarget,
-    pixelcolor::Rgb565,
-    prelude::*,
-    primitives::{PrimitiveStyleBuilder, Rectangle},
-};
+use embedded_graphics::{draw_target::DrawTarget, image::Image, pixelcolor::Rgb565, prelude::*};
 use embedded_hal::digital::v2::OutputPin;
 use esp_idf_hal::{
-    gpio::{AnyIOPin, Gpio16, Gpio18, Gpio19, Gpio23, Gpio4, Gpio5, PinDriver},
+    gpio::{AnyIOPin, Gpio0, Gpio16, Gpio18, Gpio19, Gpio23, Gpio4, Gpio5, PinDriver},
     peripheral::PeripheralRef,
     spi::{config::Config, Dma, SpiDeviceDriver, SpiDriver, SPI2},
-    units::{FromValueType, Hertz},
+    units::FromValueType,
 };
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use mipidsi::{models::Model, Builder, Display};
+use mipidsi::{
+    models::{Model, ST7789},
+    Builder, Display, ModelOptions,
+};
+use tinybmp::Bmp;
+
+// Defines the IMAGE_DATA variable, set up by build.rs
+include!(concat!(env!("OUT_DIR"), "/images.rs"));
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -29,18 +31,50 @@ fn main() {
     }
 }
 
-const TFT_WIDTH: usize = 240;
-const TFT_HEIGHT: usize = 320;
+const TFT_WIDTH: usize = 135;
+const TFT_HEIGHT: usize = 240;
 
-const BLUE: Rgb565 = Rgb565::new(91, 206, 250);
-const PINK: Rgb565 = Rgb565::new(245, 169, 184);
-const WHITE: Rgb565 = Rgb565::new(255, 255, 255);
+/// Implementation of `mipidsi::Model` for the Lilygo ttgo t-display microcontroller's display. This is fundamentally a
+struct TDisplayModel;
+
+impl Model for TDisplayModel {
+    type ColorFormat = <ST7789 as Model>::ColorFormat;
+
+    fn init<RST, DELAY, DI>(
+        &mut self,
+        di: &mut DI,
+        delay: &mut DELAY,
+        madctl: u8,
+        rst: &mut Option<RST>,
+    ) -> Result<u8, mipidsi::error::InitError<RST::Error>>
+    where
+        RST: OutputPin,
+        DELAY: embedded_hal::prelude::_embedded_hal_blocking_delay_DelayUs<u32>,
+        DI: WriteOnlyDataCommand,
+    {
+        let mut s = ST7789;
+        s.init(di, delay, madctl, rst)
+    }
+
+    fn write_pixels<DI, I>(&mut self, di: &mut DI, colors: I) -> Result<(), mipidsi::Error>
+    where
+        DI: WriteOnlyDataCommand,
+        I: IntoIterator<Item = Self::ColorFormat>,
+    {
+        let mut s = ST7789;
+        s.write_pixels(di, colors)
+    }
+
+    fn default_options() -> ModelOptions {
+        ModelOptions::with_all((135, 240), (135, 240), |_| (52, 40))
+    }
+}
 
 fn init_display() -> Result<
     Display<impl WriteOnlyDataCommand, impl Model<ColorFormat = Rgb565>, impl OutputPin>,
     &'static str,
 > {
-    let mut delay = esp_idf_hal::delay::FreeRtos;
+    let mut delay = esp_idf_hal::delay::Ets;
 
     let sclk = unsafe { Gpio18::new() };
 
@@ -73,7 +107,7 @@ fn init_display() -> Result<
         .map_err(|_| "gpio23: create pin driver")?;
 
     let di = SPIInterfaceNoCS::new(device_driver, dc);
-    let mut display = Builder::st7789(di)
+    let mut display = Builder::with_model(di, TDisplayModel)
         .init(&mut delay, Some(rst))
         .map_err(|_| "init display")?;
 
@@ -88,22 +122,61 @@ fn init_display() -> Result<
 fn run() -> Result<(), &'static str> {
     let mut display = init_display()?;
 
-    let mut rect = Rectangle::new(Point::new(48, 0), Size::new(24, 320));
+    let bmps = IMAGE_DATA
+        .into_iter()
+        .map(|bytes| Bmp::from_slice(bytes).unwrap())
+        .collect::<Vec<_>>();
 
-    for color in [
-        Rgb565::CSS_AQUA,
-        Rgb565::CSS_HOT_PINK,
-        Rgb565::WHITE,
-        Rgb565::CSS_HOT_PINK,
-        Rgb565::CSS_AQUA,
-    ] {
-        let start = Instant::now();
-        display.fill_solid(&rect, color).map_err(|_| "draw")?;
+    let changer =
+        PinDriver::input(unsafe { Gpio0::new() }).map_err(|_| "gpio0: create pin driver")?;
 
-        println!("color {:?} {:?}", color, start.elapsed());
+    let bmps_len = bmps.len();
 
-        rect.translate_mut(Point::new(24, 0));
+    let mut bmp_idx = 0;
+
+    let point = Point::new(0, 0);
+
+    display.clear(Rgb565::WHITE).map_err(|_| "clear")?;
+    Image::new(&bmps[0], point)
+        .draw(&mut display)
+        .map_err(|_| "draw image")?;
+
+    let mut debounce_time = Instant::now();
+    let mut held = false;
+
+    loop {
+        if changer.is_low() {
+            if !held && debounce_time.elapsed() > Duration::from_millis(250) {
+                debounce_time = Instant::now();
+                held = true;
+
+                bmp_idx = (bmp_idx + 1) % bmps_len;
+
+                Image::new(&bmps[bmp_idx], point)
+                    .draw(&mut display)
+                    .map_err(|_| "draw image")?;
+            }
+        } else {
+            held = false;
+        }
     }
 
-    Ok(())
+    // let mut rect = Rectangle::new(Point::new(48, 0), Size::new(24, 320));
+
+    // for color in [
+    //     Rgb565::CSS_AQUA,
+    //     Rgb565::CSS_HOT_PINK,
+    //     Rgb565::WHITE,
+    //     Rgb565::CSS_HOT_PINK,
+    //     Rgb565::CSS_AQUA,
+    // ] {
+    //     let start = Instant::now();
+    //     display.fill_solid(&rect, color).map_err(|_| "draw")?;
+
+    //     println!("color {:?} {:?}", color, start.elapsed());
+
+    //     rect.translate_mut(Point::new(24, 0));
+    // }
+
+    // Ok(())
 }
